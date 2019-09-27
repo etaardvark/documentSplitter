be09269d4a98aa130a4746ea6ade4e69257a7206
etaardvark <shdgfx@blueyonder.co.uk>
2019-09-27 21:46:17 +0100

    initial commit

---
+++ documentSplitter/documentSplitter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace documentSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!System.IO.File.Exists(args[0]))
+            {
+                Console.WriteLine("Input file does not exist ");
+                Environment.Exit(-1);
+            }
+            if(!System.IO.Directory.Exists(args[1]))
+            {
+                Console.WriteLine("Output directory does not exist");
+                Environment.Exit(-1);
+            }
+
+            documentSplitter splitter = new documentSplitter(args[0], args[1]);
+            splitter.SplitDocument();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ documentSplitter/documentSplitter/documentSplitter.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+using DocumentFormat.OpenXml.Validation;
+using OpenXmlPowerTools;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace documentSplitter
+{
+    public class documentSplitter
+    {
+        WmlDocument inDoc;
+        string _outPath;
+        int sectionCounter = 0;
+        public documentSplitter(string inputDocument, string outputPath)
+        {
+            inDoc = new WmlDocument(inputDocument);
+            _outPath = outputPath;
+        }
+
+        public void SplitDocument()
+        {
+            WmlDocument wmlDocument = new WmlDocument(inDoc.FileName);
+            IEnumerable<WmlDocument> wmlDocuments = wmlDocument.SplitOnSections();
+            int counter = 0;
+            foreach(WmlDocument doc in wmlDocuments)
+            {
+                List<Source> sources = new List<Source>();
+                sources.Add(new Source(doc));
+                DocumentBuilder.BuildDocument(sources, System.IO.Path.Combine(_outPath, $"{counter}.docx"));
+                counter++;
+            }
+        }
+    }
+}
